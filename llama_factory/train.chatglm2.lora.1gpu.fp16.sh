3580a6cf4d902b3770e8e1559e24760f5dbc690a
ybdesire <ybdesire@gmail.com>
2023-11-10 18:31:31 +0800

    Create train.chatglm2.lora.1gpu.fp16.sh

---
+++ llama_factory/train.chatglm2.lora.1gpu.fp16.sh
@@ -0,0 +1,19 @@
+CUDA_VISIBLE_DEVICES=0 python /aaa/bbb/ccc/LLaMAFactory/LLaMA-Factory/src/train_bash.py \
+    --stage sft \
+    --model_name_or_path /aaa/bbb/ccc/LLaMAFactory/LLaMA-Factory/models/dataroot/models/THUDM/chatglm2-6b \
+    --do_train \
+    --dataset my_dataset \
+    --template chatglm2 \
+    --finetuning_type lora \
+    --lora_target query_key_value \
+    --output_dir output/chatglm2_sft_lora_my/ \
+    --overwrite_cache \
+    --per_device_train_batch_size 4 \
+    --gradient_accumulation_steps 4 \
+    --lr_scheduler_type cosine \
+    --logging_steps 10 \
+    --save_steps 10 \
+    --learning_rate 5e-5 \
+    --num_train_epochs 3.0 \
+    --plot_loss \
+    --fp16
