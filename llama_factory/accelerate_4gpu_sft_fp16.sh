279867470f5f60c13bc1a650ce27cc7d4cb9b2a6
ybdesire <ybdesire@gmail.com>
2023-11-24 18:57:03 +0800

    Create accelerate_4gpu_sft_fp16.sh

---
+++ llama_factory/accelerate_4gpu_sft_fp16.sh
@@ -0,0 +1,20 @@
+accelerate launch   --multi_gpu --mixed_precision=fp16 --num_processes=4   src/train_bash.py \
+    --stage sft \
+    --model_name_or_path models/chatglm3-6b \
+    --do_train \
+    --dataset aaa,bbb \
+    --template chatglm3 \
+    --finetuning_type lora \
+    --lora_target query_key_value \
+    --output_dir output/abcabc/ \
+    --overwrite_cache \
+    --per_device_train_batch_size 4 \
+    --gradient_accumulation_steps 4 \
+    --lr_scheduler_type cosine \
+    --logging_steps 10 \
+    --save_steps 200 \
+    --learning_rate 5e-5 \
+    --num_train_epochs 200 \
+    --plot_loss \
+    --overwrite_output_dir True \
+    --fp16
