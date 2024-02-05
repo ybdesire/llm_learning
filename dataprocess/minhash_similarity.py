48262f7c9da40f4b8850c7ef44a7323e5643f68d
ybdesire <ybdesire@gmail.com>
2024-02-05 17:11:19 +0800

    Create minhash_similarity.py

---
+++ dataprocess/minhash_similarity.py
@@ -0,0 +1,20 @@
+from datasketch import MinHash
+# data should like this
+data1 = ['minhash', 'is', 'a', 'probabilistic', 'data', 'structure', 'for',
+        'estimating', 'the', 'similarity', 'between', 'datasets']
+data2 = ['minhash', 'is', 'a', 'probability', 'data', 'structure', 'for',
+        'estimating', 'the', 'similarity', 'between', 'documents']
+# num_perm for accuracy
+m1, m2 = MinHash(num_perm=128), MinHash(num_perm=128)
+# update() to calculate minhash
+for d in data1:
+    m1.update(d.encode('utf8'))
+for d in data2:
+    m2.update(d.encode('utf8'))
+# Estimated Jaccard similarity
+print("Estimated Jaccard for data1 and data2 is", m1.jaccard(m2))#0.7109375
+# Actual Jaccard similarity
+s1 = set(data1)
+s2 = set(data2)
+actual_jaccard = float(len(s1.intersection(s2)))/float(len(s1.union(s2)))
+print("Actual Jaccard for data1 and data2 is", actual_jaccard)#0.7142857142857143
