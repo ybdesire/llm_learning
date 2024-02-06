46d864cc25b35baeba47a0c1ad5a78378b0feda5
ybdesire <ybdesire@gmail.com>
2024-02-06 16:36:15 +0800

    add prompt prefix for llm sft dataset

---
+++ dataprocess/prompt_prefix_en.py
@@ -0,0 +1,28 @@
+# reference: https://github.com/charent/ChatLM-mini-Chinese/blob/main/utils/raw_data_process.py#L617
+# will keep updating
+
+prompt_prefix = [
+'What is {0}?',
+'Introduce {0}',
+'Tell me about {0}',
+'Write an introduction about {0}',
+'What is {0}?',
+'Do you know {0}?',
+'Generate an introduction for {0}',
+'I want to know detailed information about {0}',
+'Do you know about {0}?',
+'Please explain {0}',
+'What do you know or think about {0}?',
+'Tell me some information about {0}',
+'Please briefly describe {0}',
+'Please provide some detailed information about {0}',
+'Can you explain what {0} is?',
+'Please share some background knowledge about {0}',
+'Please briefly summarize {0}',
+'Can you give me some background information about {0}?',
+'Do you have any information to share about {0}?',
+'Can you tell me what {0} is?',
+'Please describe the main features of {0}',
+'How can I better understand {0}?',
+'Describe the basic concept of {0}',
+]
